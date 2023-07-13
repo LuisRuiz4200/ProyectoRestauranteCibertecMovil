39a69b95b6eaf995da199117a958728d4f3c3f7d
LuisRuiz4200 <lr27072001@gmail.com>
2023-07-12 19:24:54 -0500

    ok

---
+++ PJ_RESTAURANTE_5TO_CICLO/restaurante/app/src/main/java/com/example/restaurante/model/Colaborador.kt
@@ -0,0 +1,13 @@
+package com.example.restaurante.model
+
+class Colaborador {
+
+    private var id_colaborador : Int = 0
+    private var id_tipo_colaborador : Int =0
+    private var nom_colaborador : String = ""
+    private var ape_colaborador : String = ""
+    private var dni_colaborador : String = ""
+    private var imagen_colabordar : Byte = 0
+
+
+}
