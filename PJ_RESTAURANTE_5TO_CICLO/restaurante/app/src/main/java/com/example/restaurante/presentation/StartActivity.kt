ab527dfe38306ed97db02d1f90f62e2515295776
mysticBel <dnxbel@gmail.com>
2023-07-21 12:56:23 -0500

    layout activity_start

---
+++ PJ_RESTAURANTE_5TO_CICLO/restaurante/app/src/main/java/com/example/restaurante/presentation/StartActivity.kt
@@ -0,0 +1,12 @@
+package com.example.restaurante.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.restaurante.R
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
