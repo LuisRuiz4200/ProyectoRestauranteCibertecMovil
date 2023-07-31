2efeab826b6bf7ddcecf9f3cab212bdc66c93f6d
sadohu <sadohu26@yahoo.com>
2023-07-30 22:48:15 -0500

    Diseño AgregarTarjetaActivity

---
+++ PJ_RESTAURANTE_5TO_CICLO/restaurante/app/src/main/java/com/example/restaurante/presentation/Perfil/MisTarjetas/AgregarTarjeta/AgregarTarjetaActivity.kt
@@ -0,0 +1,20 @@
+package com.example.restaurante.presentation.Perfil.MisTarjetas.AgregarTarjeta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.restaurante.databinding.ActivityAgregarTarjetaBinding
+
+class AgregarTarjetaActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityAgregarTarjetaBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAgregarTarjetaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initValues()
+    }
+
+    private fun initValues() {
+        TODO("Not yet implemented")
+    }
+}
