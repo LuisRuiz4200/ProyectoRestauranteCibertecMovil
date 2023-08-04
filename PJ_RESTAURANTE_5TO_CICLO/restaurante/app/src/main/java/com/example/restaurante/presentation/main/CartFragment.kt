ddf461a70fdee8ce3e89adcdb64769d1f4df5457
sadohu <sadohu26@yahoo.com>
2023-08-03 20:55:05 -0500

    Calcular y mostrar precio total en CartFragment

--- PJ_RESTAURANTE_5TO_CICLO/restaurante/app/src/main/java/com/example/restaurante/presentation/main/CartFragment.kt
+++ PJ_RESTAURANTE_5TO_CICLO/restaurante/app/src/main/java/com/example/restaurante/presentation/main/CartFragment.kt
@@ -9,17 +9,16 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.restaurante.R
 import com.example.restaurante.data.room.BDPolleria
 import com.example.restaurante.data.room.entity.Cart
-import com.example.restaurante.data.room.entity.Producto
-import com.example.restaurante.domain.viewmodel.ProductoViewModel
 import com.example.restaurante.presentation.cart.CartAdapter
 import kotlinx.android.synthetic.main.activity_cart.view.rvCart
+import kotlinx.android.synthetic.main.activity_cart.view.tvPrecio
 
 class CartFragment : Fragment() {
-    private lateinit var viewModel: ProductoViewModel // <---Crear CartViewModel
+//    private lateinit var viewModel: ProductoViewModel // <---Crear CartViewModel
     private lateinit var database : BDPolleria
     private lateinit var cartAdapter : CartAdapter
     private var cart : MutableList<Cart> = ArrayList()
-    private var productos : MutableList<Producto> = ArrayList()
+//    private var productos : MutableList<Producto> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,13 +31,17 @@ class CartFragment : Fragment() {
     }
 
     private fun initValues(view: View) {
-        database = BDPolleria.getInstancia(view.context)
+        database = BDPolleria.getInstancia(requireContext())
         cart = database.cartDao().getAll()
         cartAdapter = CartAdapter(cart)
-        view.rvCart.layoutManager = LinearLayoutManager(view.context)
+        view.rvCart.layoutManager = LinearLayoutManager(requireContext())
         view.rvCart.adapter = cartAdapter
-
-
+        var totalPrecio = 0.0
+        for (item in cart){
+            val totalPorProducto = item.cantidad_producto * item.preciouni_producto
+            totalPrecio += totalPorProducto
+        }
+        view.tvPrecio.text = String.format("%.2f",totalPrecio)
     }
 
     private fun initObservers() {
