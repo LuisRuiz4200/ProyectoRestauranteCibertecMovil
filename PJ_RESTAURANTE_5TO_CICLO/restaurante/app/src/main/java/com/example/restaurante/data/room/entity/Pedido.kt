6f7deb2d2b8716cf9948b80b135bab6a816e6c34
sadohu <sadohu26@yahoo.com>
2023-08-15 18:05:12 -0500

    Update: Aumentar campo Apellido y Scroll View - EditarPerfilActivity

--- PJ_RESTAURANTE_5TO_CICLO/restaurante/app/src/main/java/com/example/restaurante/data/room/entity/Pedido.kt
+++ PJ_RESTAURANTE_5TO_CICLO/restaurante/app/src/main/java/com/example/restaurante/data/room/entity/Pedido.kt
@@ -8,15 +8,15 @@ import androidx.room.TypeConverters
 @TypeConverters(CartListTypeConverter::class)
 data class Pedido(
     @PrimaryKey(autoGenerate = true)
-    var id_pedido: Int = 0,
-    var id_usuario_cliente: Int = 0,
-    var id_dirEntrega: Int = 0,
+    var id_pedido: Int = 0,             // ----------------
+    var id_usuario_cliente: Int = 0,    // ----------------
+    var id_dirEntrega: Int = 0,         // ----------------
     var id_colaborador_repartidor: Int = 0,
     var tiempoEntrega_pedido : String = "",
-    var id_tarjeta: Int = 0,
-    var id_medio_pago: Int = 0,
-    var monto_compra: Double = 0.0,
-    var carts: List<Cart> = emptyList(),
+    var id_tarjeta: Int = 0,            // ----------------
+    var id_medio_pago: Int = 0,         // ----------------
+    var monto_compra: Double = 0.0,     // ----------------
+    var carts: List<Cart> = emptyList(),    // ----------------
     var fechaReg_pedido : String = "",
     var fechaAct_pedido : String = "",
     var estado_pedido: String = ""
--- PJ_RESTAURANTE_5TO_CICLO/restaurante/app/src/main/java/com/example/restaurante/presentation/perfil/EditarPerfil/EditarPerfilActivity.kt
+++ PJ_RESTAURANTE_5TO_CICLO/restaurante/app/src/main/java/com/example/restaurante/presentation/perfil/EditarPerfil/EditarPerfilActivity.kt
@@ -3,8 +3,13 @@ package com.example.restaurante.presentation.perfil.EditarPerfil
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.restaurante.data.preference.SharedPreferences
 import com.example.restaurante.databinding.ActivityEditarPerfilBinding
 import com.example.restaurante.presentation.perfil.CambiarContrasena.CambiarContrasenaActivity
+import kotlinx.android.synthetic.main.activity_perfil_usuario.view.tvEmail
+import kotlinx.android.synthetic.main.activity_perfil_usuario.view.tvNombres
+import kotlinx.android.synthetic.main.activity_perfil_usuario.view.tvNumero
 
 class EditarPerfilActivity : AppCompatActivity() {
     private lateinit var binding : ActivityEditarPerfilBinding
@@ -17,10 +22,19 @@ class EditarPerfilActivity : AppCompatActivity() {
     }
 
     private fun initValues() {
+        setProfile()
+
         binding.btnCambiarContrasena.setOnClickListener {
             startActivity(Intent(this,CambiarContrasenaActivity::class.java))
         }
         var id = intent.getIntExtra("id", 0)
         println("id $id")
+    }
+
+    private fun setProfile() {
+//        val usuario = SharedPreferences.getPrefUsuario(requireContext())!!
+//        "${usuario.nom_usuario} ${usuario.ape_usuario}".also { view.tvNombres.text = it }
+//        view.tvEmail.text = usuario.email_usuario
+//        view.tvNumero.text = usuario.cel_usuario
     }
 }
